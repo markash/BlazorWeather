ebe79ad07c5f0a252be9946775e824c21b86fe5e
Mark Ashworth <mp.ashworth@gmail.com>
2021-09-23 16:52:59 +0200

    Spelling fix

--- Api/api.cs
+++ Api/api.cs
@@ -12,7 +12,7 @@ namespace Weather.Function
 {
     public static class api
     {
-        [FunctionName("CurrentConsitions")]
+        [FunctionName("CurrentConditions")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
